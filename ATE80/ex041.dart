9315d296a8b9ba472e45fa4914b6d8d82a9eace8
Josivan da S. Marques <72993762+josivanphs@users.noreply.github.com>
2023-06-06 10:41:59 -0400

    ex041

--- ATE80/ex041.dart
+++ ATE80/ex041.dart
@@ -1,1 +1,17 @@
+import 'dart:io';
 
+main() {
+  int a, b, c, d;
+  double mp;
+  print('Entre com 1 número: ');
+  a = int.parse(stdin.readLineSync()!);
+  print('Entre com 2 número: ');
+  b = int.parse(stdin.readLineSync()!);
+  print('Entre com 3 número: ');
+  c = int.parse(stdin.readLineSync()!);
+  print('Entre com 4 número: ');
+  d = int.parse(stdin.readLineSync()!);
+
+  mp = (a * 1 + b * 2 + c * 3 + d * 4) / 10;
+  print(mp);
+}
